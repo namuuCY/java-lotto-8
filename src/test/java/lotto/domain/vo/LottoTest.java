7290f541416eb517097b808cf69fd5387465861d
namucy <wkdcjfdud13@gmail.com>
2025-10-31 16:36:47 +0900

    test(domain): LottoTest 작성
    - Lotto의 단위 테스트인 LottoTest를 작성합니다.

--- src/test/java/lotto/domain/vo/LottoTest.java
+++ src/test/java/lotto/domain/vo/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain.vo;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,4 +22,19 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    @DisplayName("로또 번호가 주어진 범위를 넘으면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_주어진_범위를_넘으면_예외() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 55)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("번호 순서가 오름차순이 아니면 예외가 발생한다")
+    @Test
+    void 번호_순서가_오름차순이_아니면_예외() {
+        assertThatThrownBy(() -> new Lotto(List.of(5, 2, 9, 7, 3, 1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
