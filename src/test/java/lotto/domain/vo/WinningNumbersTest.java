6bdcae68ccc4234e20d8b71554c191fa5b09b428
namucy <wkdcjfdud13@gmail.com>
2025-11-02 14:38:17 +0900

    test(domain): WinningNumbers 테스트 작성
    - WinningNumbers의 테스트 코드를 작성합니다.

---
+++ src/test/java/lotto/domain/vo/WinningNumbersTest.java
@@ -0,0 +1,22 @@
+package lotto.domain.vo;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningNumbersTest {
+
+    @DisplayName("보너스 번호와 로또 번호가 중복될 경우 예외 발생")
+    @Test
+    void 보너스_번호_로또_번호_중복_시_예외_발생() {
+        assertThatThrownBy(() -> {
+                    WinningNumbers.of(
+                            List.of(1, 3, 4, 7, 9, 13),
+                            13
+                    );
+                }
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
