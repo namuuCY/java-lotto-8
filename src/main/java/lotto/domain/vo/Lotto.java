2e4588bde44b003164c71b5a6606a8db379dd0ea
namucy <wkdcjfdud13@gmail.com>
2025-11-02 14:27:48 +0900

    feat(domain): Lotto 구현
    - 도메인 관련 값 객체인 Lotto를 구현합니다.
    - Lotto 관련 테스트 코드를 추가 작성합니다.

---
+++ src/main/java/lotto/domain/vo/Lotto.java
@@ -0,0 +1,87 @@
+package lotto.domain.vo;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.exception.DomainException;
+import lotto.exception.ExceptionCode;
+
+// 이 클래스를 사용해야 한다.
+public class Lotto {
+    // 아래 필드의 접근제어자는 private 이어야 하며, 이 필드 이외의 필드가 있어서는 안된다.
+    private final List<Integer> numbers;
+
+    private Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    public static Lotto of(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateCombination(numbers);
+        validateElements(numbers);
+    }
+
+    // TODO: 추가 기능 구현
+
+    private void validateCombination(List<Integer> numbers) {
+        validateLength(numbers);
+        validateDuplicate(numbers);
+        validateAscending(numbers);
+    }
+
+    private void validateAscending(List<Integer> numbers) {
+        Integer numbersSize = numbers.size();
+        for (int i = 0; i < numbersSize - 2; i++) {
+            isAscending(numbers.get(i), numbers.get(i + 1));
+        }
+    }
+
+    private void isAscending(Integer former, Integer latter) {
+        if (former < latter) {
+            return;
+        }
+        throw new DomainException(ExceptionCode.LOTTO_NUMBERS_NOT_ASCENDING);
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Integer numbersSize = numbers.size();
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        Integer distinctNumbersSize = distinctNumbers.size();
+        judgeDuplicate(numbersSize, distinctNumbersSize);
+    }
+
+    private void judgeDuplicate(Integer numbersSize, Integer distinctNumbersSize) {
+        if (numbersSize.equals(distinctNumbersSize)) {
+            return;
+        }
+        throw new DomainException(ExceptionCode.LOTTO_NUMBERS_DUPLICATED);
+    }
+
+    private void validateLength(List<Integer> numbers) {
+        if (numbers.size() == 6) {
+            return;
+        }
+        throw new DomainException(ExceptionCode.NOT_ENOUGH_LOTTO_NUMBERS);
+    }
+
+
+    private void validateElements(List<Integer> numbers) {
+        numbers.forEach(this::checkOutOfBound);
+    }
+
+    private void checkOutOfBound(Integer number) {
+        if (number >= 1 && number <= 45) {
+            return;
+        }
+        throw new DomainException(ExceptionCode.OUT_OF_BOUND_NUMBER);
+    }
+
+    public Boolean isIncluding(Integer target) {
+        return numbers.stream()
+                .anyMatch(number -> number.equals(target));
+    }
+}
--- src/test/java/lotto/domain/vo/LottoTest.java
+++ src/test/java/lotto/domain/vo/LottoTest.java
@@ -1,23 +1,27 @@
 package lotto.domain.vo;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> Lotto.of(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> Lotto.of(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -26,15 +30,33 @@ class LottoTest {
     @DisplayName("로또 번호가 주어진 범위를 넘으면 예외가 발생한다.")
     @Test
     void 로또_번호가_주어진_범위를_넘으면_예외() {
-        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 55)))
+        assertThatThrownBy(() -> Lotto.of(List.of(0, 2, 3, 4, 5, 55)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("번호 순서가 오름차순이 아니면 예외가 발생한다")
     @Test
     void 번호_순서가_오름차순이_아니면_예외() {
-        assertThatThrownBy(() -> new Lotto(List.of(5, 2, 9, 7, 3, 1)))
+        assertThatThrownBy(() -> Lotto.of(List.of(5, 2, 9, 7, 3, 1)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("특정 숫자가 조합에 포함되어 있는지 테스트")
+    @ParameterizedTest
+    @MethodSource("generateMockData")
+    void 특정_숫자가_조합에_포함되어_있는지_테스트(List<Integer> numbers, Integer target, Boolean expected) {
+        // given
+        Lotto lotto = Lotto.of(numbers);
+        // when
+        Boolean result = lotto.isIncluding(target);
+        // then
+        assertThat(expected).isEqualTo(result);
+    }
+
+    static Stream<Arguments> generateMockData() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6), 3, true),
+                Arguments.of(List.of(11, 22, 33, 34, 35, 36), 7, false)
+        );
+    }
 }
