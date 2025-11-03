304505185e2c1f72c37f0742b2a9b540f2f8efa5
namucy <wkdcjfdud13@gmail.com>
2025-11-03 18:07:20 +0900

    test(domain): CustomerTest 테스트 코드 작성
    - CustomerTest의 Customer 테스트 코드를 작성합니다.
    - 랜덤 값을 넣는 대신, 원하는 로또 번호를 집어넣기 위한 StubLottoGenerator를 통해서 로또를 주입하는 코드를 작성합니다.

--- src/test/java/lotto/domain/entity/CustomerTest.java
+++ src/test/java/lotto/domain/entity/CustomerTest.java
@@ -1,7 +1,13 @@
 package lotto.domain.entity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
 import java.util.List;
+import lotto.domain.service.LottoGenerator;
+import lotto.domain.service.StubLottoGenerator;
 import lotto.domain.vo.Lotto;
+import lotto.domain.vo.Rank;
 import lotto.domain.vo.Statistics;
 import lotto.domain.vo.WinningLotto;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,8 +24,12 @@ public class CustomerTest {
         Lotto lotto1 = Lotto.of(List.of(1, 2, 3, 10, 11, 12)); // 3개 일치 (5등)
         Lotto lotto2 = Lotto.of(List.of(1, 2, 3, 4, 10, 11)); // 4개 일치 (4등)
         Lotto lotto3 = Lotto.of(List.of(10, 11, 12, 13, 14, 15)); // 0개 일치 (꽝)
+        LottoGenerator stub = new StubLottoGenerator(
+                Arrays.asList(lotto1, lotto2, lotto3)
+        );
+        customer = Customer.from(3000);
 
-        customer = new Customer(List.of(lotto1, lotto2, lotto3));
+        customer.purchaseLotto(stub);
 
         // 당첨 번호: 1, 2, 3, 4, 5, 6 + 보너스 7
         winningLotto = WinningLotto.of(
@@ -30,15 +40,15 @@ public class CustomerTest {
 
     @Test
     void 당첨_번호와_비교하여_통계를_정확히_생성() {
-        // when
-        Statistics statistics = customer.calculateStatistics(winningLotto);
+//         when
+        Statistics statistics = customer.aggregateResults(winningLotto);
 
         // then
-        assertThat(statistics.getCount(Rank.FIFTH)).isEqualTo(1);
-        assertThat(statistics.getCount(Rank.FOURTH)).isEqualTo(1);
-        assertThat(statistics.getCount(Rank.MISS)).isEqualTo(1);
-        assertThat(statistics.getCount(Rank.FIRST)).isEqualTo(0);
-        assertThat(statistics.getCount(Rank.SECOND)).isEqualTo(0);
-        assertThat(statistics.getCount(Rank.THIRD)).isEqualTo(0);
+        assertThat(statistics.showCounts(Rank.FIFTH)).isEqualTo(1);
+        assertThat(statistics.showCounts(Rank.FOURTH)).isEqualTo(1);
+        assertThat(statistics.showCounts(Rank.MISS)).isEqualTo(1);
+        assertThat(statistics.showCounts(Rank.FIRST)).isEqualTo(0);
+        assertThat(statistics.showCounts(Rank.SECOND)).isEqualTo(0);
+        assertThat(statistics.showCounts(Rank.THIRD)).isEqualTo(0);
     }
 }
