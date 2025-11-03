ace1caf0493fe2d1182caeb85fcd9c62b6429181
namucy <wkdcjfdud13@gmail.com>
2025-11-03 20:10:29 +0900

    fix(domain): Rank 기능 추가
    - Rank 관련 메세지를 포함하도록 필드를 추가하고, 생성자를 수정합니다.
    - 관련 Message를 볼 수 있도록 getter를 추가합니다.

--- src/main/java/lotto/domain/vo/Rank.java
+++ src/main/java/lotto/domain/vo/Rank.java
@@ -1,17 +1,19 @@
 package lotto.domain.vo;
 
 public enum Rank {
-    FIRST(2_000_000_000L),
-    SECOND(30_000_000L),
-    THIRD(1_500_000L),
-    FOURTH(50_000L),
-    FIFTH(5_000L),
-    MISS(0L);
+    FIRST(2_000_000_000L, "6개 일치"),
+    SECOND(30_000_000L, "5개 일치, 보너스 볼 일치"),
+    THIRD(1_500_000L, "5개 일치"),
+    FOURTH(50_000L, "4개 일치"),
+    FIFTH(5_000L, "3개 일치"),
+    MISS(0L, "낙첨"); // MISS도 설명을 가질 수 있음
 
     private final Long prizeMoney;
+    private final String description; // 2. 필드 선언
 
-    Rank(Long prizeMoney) {
+    Rank(Long prizeMoney, String description) { // 3. 생성자 수정
         this.prizeMoney = prizeMoney;
+        this.description = description;
     }
 
     public static Rank valueOf(Integer matchCount, Boolean bonusMatch) {
@@ -43,5 +45,9 @@ public enum Rank {
 
     public Long getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
